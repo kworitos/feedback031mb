779b318673ac57b2aabd10e1b0feec4d74f70a38
mheloseco <mheloseco@gmail.com>
2025-04-15 23:16:31 +0000

    mucança da funçao handleSubmit para o CardInicial; alteracao da duraçao do hover dos botoes

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,16 @@ import {useState} from "react"
 import { CardInicial } from "./components/CardInicial"
 import { CardFinal } from "./components/CardFinal"
 
-
 export function App(){
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
   const[submited, setSubmited] = useState(false)
 
-
-
-  function handleSubmit(){
-    if (notaAvaliacao !== 0){
-      setSubmited(true)
-      return
-    }
-
-    alert("Please, choose a note!")
-  }
-
   return(
     submited === false ? (
-
       <CardInicial setNotaAvaliacao={setNotaAvaliacao}
-      handleSubmit={handleSubmit}/>
-
+      setSubmited={setSubmited} notaAvaliacao={notaAvaliacao}/>
     ) :(
-      <CardFinal notaAvaliacao={notaAvaliacao}/>
+      <CardFinal notaAvaliacao={notaAvaliacao} />
     )
   )
 }
